1d538cf431075147cc6c2a5f4a9b889ad9503389
toimi68 <45996495+toimi68@users.noreply.github.com>
2019-04-02 16:59:30 +0200

    php
    02.04

--- PHP/post/formulaire1.php
+++ PHP/post/formulaire1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,7 +22,7 @@
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
 
-<button class="btn btn-primary" type="submit">Button</button>
+<button class="btn btn-primary" type="submit">submit</button>
 
 
 
